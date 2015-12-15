c38baff10c4e37152ae05e5e57b44887106c0101
CristhianF <CristhianF@MacCristhianF.local>
2015-12-14 23:01:25 -0500

    Ajustes Planes

--- src/js/controllers/signupDetails.js
+++ src/js/controllers/signupDetails.js
@@ -14,6 +14,14 @@ app.controller('SignUpDetails', ['$scope', '$http', '$state', function($scope, $
         { descripcion: 'Educación especial',     codigo: 'EE'},
         ];
     
+    $scope.planes = [
+        { descripcion : "Básico", rangoEstudiantes : "0 - 1000", precioMensual : "150.000",
+            precioSemestral : "800.000", precioAnual : "1'400.000", clase : "block panel padder-v bg-primary item" },
+        { descripcion : "Normal", rangoEstudiantes : "1000 - 3000", precioMensual : "150.000",
+            precioSemestral : "800.000", precioAnual : "1'400.000", clase : "block panel padder-v bg-primary item" }
+    ]
+    
+    
     $scope.tipoInstitucion = {};
     $scope.tipoJornada = {};
     $scope.ubicacion = {};
